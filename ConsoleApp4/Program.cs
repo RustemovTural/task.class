e608508d459e1cb107ff7731f69399ff1faca74c
RustemovTural <tu80rq7h6@code.edu.az>
2024-03-25 23:00:34 +0400

    class

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp4
+{ class Vehicle
+    {
+        public string Brand;
+       public string Model;
+        public double  Mile;
+    };
+   
+    class Car : Vehicle
+    {
+       public int DoorsNum;
+        public void ShowFullInfo()
+        {
+            Console.WriteLine(Brand+" "+Model+" "+Mile+" "+DoorsNum);
+        }
+        
+    };
+    class MotorCycle : Vehicle
+    {
+       public int WhelNum;
+        public void ShowFullInfo()
+        {
+            Console.WriteLine(Brand+" "+Model+" "+Mile+" "+WhelNum);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle();
+            vehicle1.Brand = "brand";
+            vehicle1.Model = "model";
+            vehicle1.Mile= 0;
+
+            Car car1 = new Car();
+            car1.DoorsNum = 4;
+            car1.Brand = "hyundai";
+            car1.Model = "sonata";
+            car1.Mile = 90000;
+
+            MotorCycle cycle1 = new MotorCycle();
+            cycle1.WhelNum = 17;
+            cycle1.Brand = "bmw";
+            cycle1.Mile = 0;
+            cycle1.Model = "fasting";
+
+               
+           
+            car1.ShowFullInfo();
+            cycle1.ShowFullInfo();
+        }
+    }
+}
---
+++ task2 class/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace task2_class
+{class Product
+    {
+        public string Name;
+        public string Description;
+        public int Count;
+        public bool IsStock;
+        public double Price;
+
+    }class Notebook : Product
+    {
+        public byte Ram;
+        public int Storage;
+        public void ShowFullData()
+        {
+            Console.WriteLine(Name + " " + Description + " " + Count + " " + Price + " " + Ram);
+        }
+
+        public void sale()
+        {
+         if (IsStock&&Count>0)
+            {
+                Console.WriteLine(Name+" "+Description+" "+Count+" "+Price+" "+Ram);
+            }
+            else
+            {
+                Console.WriteLine("stoctda yoxdur");
+            }
+           
+            
+            
+        }
+       
+    }
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "name";
+            product1.Description = "description";
+            product1.Price = 1;
+            product1.IsStock =true;
+            product1.IsStock=false;
+            product1.Count = 1;
+
+
+            Notebook notebook1 = new Notebook();
+            notebook1.Storage = 16;
+            notebook1.Ram = 32;
+            notebook1.Name = "computer";
+            notebook1.Description = "dizustu";
+            notebook1.Price = 1500;
+            notebook1.IsStock = true;
+            notebook1.Count = 0;
+
+            notebook1.ShowFullData();
+            notebook1.sale();
+        }
+    }
+}
